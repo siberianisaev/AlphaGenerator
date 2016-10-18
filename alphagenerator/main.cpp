f88333acf6a5762d7c2e73c24215466cc3f81576
Andrey Isaev <siberian.developer@gmail.com>
2016-10-18 12:45:35 +0300

    Initial Commit

---
+++ alphagenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  alphagenerator
+//
+//  Created by Andrey Isaev on 18.10.16.
+//  Copyright © 2016 JINR. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
